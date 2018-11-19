6c48bd75e2ba23527fd24673d5ca66141675fb56
Alona Voitko <alona.voytko@gmail.com>
2018-11-19 17:43:32 +0200

    New step added

--- spec/features/first_spec.rb
+++ spec/features/first_spec.rb
@@ -5,5 +5,8 @@ feature "Visit wikipedia page", js: true do
     visit 'https://en.wikipedia.org'
 
     expect(page).to have_content 'Welcome to Wikipedia'
+
+    find(:xpath, '//*[@id="mp-topbanner"]/div/div[1]/a').click
+    expect(page).to have_content 'From Wikipedia, the free encyclopedia'
   end
 end
